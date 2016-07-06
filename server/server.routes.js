27d8876f7d550b1328fd037f9033754f78be5821
Blake Hagen <blakehagen@gmail.com>
2016-07-05 19:07:01 -0600

    initial take2

--- server/server.routes.js
+++ server/server.routes.js
@@ -1,17 +1,1 @@
 'use strict';
-
-const swapiCtrl = require('./server.controller.js');
-
-module.exports = (app) => {
-
-  // ROUTES //
-  app.route('/api/v1/character/:name')
-    .get(swapiCtrl.getCharacter); // GET ONE CHARACTER //
-
-  app.route('/api/v1/characters')
-    .get(swapiCtrl.getCharacters); // GET CHARACTERS (50) and SORT //
-
-  app.route('/api/v1/planetresidents')
-    .get(swapiCtrl.getPlanetResidents); // GET PLANETS AND RESIDENTS //
-
-};
