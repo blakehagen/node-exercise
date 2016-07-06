316ef75b184d5f405aff679234c5fee87b1be0ab
Blake Hagen <blakehagen@gmail.com>
2016-07-05 20:42:00 -0600

    getting data from 1st endpoint, need to send via ejs

--- server/server.controller.js
+++ server/server.controller.js
@@ -3,3 +3,45 @@ const _        = require('lodash');
 const request  = require('request');
 const Promise  = require('bluebird');
 const BRequest = Promise.promisify(request);
+
+module.exports = {
+
+
+  // GET CHARACTER BY NAME //
+  getCharacter: (req, res) => {
+
+    let characterHash = [
+      {
+        name: 'luke',
+        id: 1
+      },
+      {
+        name: 'han',
+        id: 14
+      },
+      {
+        name: 'leia',
+        id: 5
+      }, {
+        name: 'rey',
+        id: 85
+      }
+    ];
+
+    let options = {
+      url: 'http://swapi.co/api/people/' + _.find(characterHash, {"name": req.params.name}).id
+    };
+
+    BRequest(options).then((response) => {
+      let charData = JSON.parse(response.body);
+      res.status(200).json(charData);
+    });
+  },
+
+  getCharacters: (req, res)=> {
+  },
+
+  getPlanetResidents: (req, res) => {
+  }
+
+};
--- server/server.routes.js
+++ server/server.routes.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const swapiCtrl = require('./server.controller.js');
+
+module.exports = (app) => {
+
+  app.route('/api/v1/character/:name')
+    .get(swapiCtrl.getCharacter);
+
+  app.route('/api/v1/characters')
+    .get(swapiCtrl.getCharacters);
+
+  app.route('/api/v1/planetresidents')
+    .get(swapiCtrl.getPlanetResidents);
+  
+};
